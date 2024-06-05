6d457883d87bf2fe89c5e6e11c5991c5ac5c2ff5
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-06-05 18:15:43 +0900

    첫 빌드

--- src/pages/VisitingReservation/NameInput/index.jsx
+++ src/pages/VisitingReservation/NameInput/index.jsx
@@ -1,84 +1,3 @@
-// import React, { useRef, useState } from "react";
-// import { Title } from "../../../components/reservation/atoms/Title/Title.style";
-// import { Input } from "../../../components/reservation/atoms/Input/Input.style";
-// import { Button } from "../../../components/reservation/atoms/Button/Button.style";
-// import Form from "../../../components/reservation/molecules/Form";
-// import Container from "../../../components/reservation/templates/Container";
-// import Header from "../../../components/reservation/molecules/Header";
-// import Wrapper from "../../../components/reservation/molecules/Wrapper";
-// import { useLocation, useNavigate } from "react-router-dom";
-// import { Label } from "../../../components/reservation/atoms/Label/Label.style";
-// import { FaAngleLeft } from "react-icons/fa6";
-// import { BackBar } from "../../../components/reservation/molecules/BackBar/BackBar.style";
-
-// const NameInputPage = ({ type }) => {
-//   const [name, setName] = useState("");
-
-//   const navigate = useNavigate();
-
-//   const {
-//     state: { reservation },
-//   } = useLocation();
-
-//   console.log(reservation);
-
-//   const onChange = (event) => {
-//     setName(event.target.value);
-//   };
-
-//   const onSubmit = async (event) => {
-//     event.preventDefault();
-
-//     if (type === "reservation" && name.length <= 4) {
-//       reservation.name = name;
-
-//       /*
-//       // 해당 어르신 고유 id 값 조회 API
-//       const elderlyId = await (
-//         await fetch(
-//           `https://port-0-apple-tree-v1-1mrfs72llwuqd2yb.sel5.cloudtype.app/elderly/findByName?name=${reservation.name}`
-//         )
-//       ).json();
-
-//       console.log("어르신 id 값 : ", elderlyId);
-
-//       reservation.elderlyId = elderlyId;
-//       */
-
-//       navigate("/visitingReservation/relationship-input", {
-//         state: { reservation },
-//       });
-//     } else if (type === "check" && name.length <= 4) {
-//       navigate("/reservation-check/list", {
-//         state: { name },
-//       });
-//     }
-//   };
-
-//   return (
-//     <>
-//       <Container>
-//         <BackBar onClick={() => navigate(-1)}>
-//           <FaAngleLeft />
-//         </BackBar>
-//         <Header>
-//           <Title>어르신 성함을 입력해주세요</Title>
-//         </Header>
-//         <Form onSubmit={onSubmit}>
-//           <Wrapper>
-//             <Label>이름</Label>
-//             <Input type="text" placeholder="어르신 성함" maxLength="3" onChange={onChange} />
-//           </Wrapper>
-//           <Button className={name.length <= 4 && "activate"} disabled={name.length < 3}>
-//             다음
-//           </Button>
-//         </Form>
-//       </Container>
-//     </>
-//   );
-// };
-// export default NameInputPage;
-
 import React, { useState } from "react";
 import { Title } from "../../../components/reservation/atoms/Title/Title.style";
 import { Input } from "../../../components/reservation/atoms/Input/Input.style";
@@ -166,7 +85,10 @@ const NameInputPage = ({ type }) => {
             <Input type="text" placeholder="어르신 성함" maxLength="3" onChange={onChange} value={name} required />
           </Wrapper>
           {error && <div style={{ color: "red" }}>{error}</div>}
-          <Button className={name.length <= 4 && "activate"} disabled={name.length < 3 || loading}>
+          <Button
+            className={name.length >= 3 && name.length <= 4 ? "activate" : ""}
+            disabled={name.length < 3 || name.length > 4 || loading}
+          >
             {loading ? "로딩 중..." : "다음"}
           </Button>
         </Form>
